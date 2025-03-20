11c5f827df0697a69bc65941e1b2888695a46df4
robin <comfysage@isabelroses.com>
2025-03-20 13:42:43 +0000

    feat: add debug component

--- build.js
+++ build.js
@@ -1,10 +1,14 @@
 const fs = require("fs").promises;
 const path = require("path");
+const { execSync } = require("child_process");
+
+const commit_hash = execSync("git rev-parse --short HEAD").toString().trim();
 
 const sourceFiles = [
   "src/evergarden-winter.theme.scss",
   "src/evergarden-fall.theme.scss",
   "src/evergarden-spring.theme.scss",
+  "src/evergarden-summer.theme.scss",
 ];
 
 const accents = [
@@ -27,14 +31,23 @@ const accents = [
 // read sourceFile and generate all accents for it
 async function generateAccents(sourceFilePath) {
   const _sourceFilePath = path.join(__dirname, sourceFilePath);
-  const sourceFileData = await fs.readFile(_sourceFilePath, {
-    encoding: "utf8",
-  });
-  return Promise.all(
-    accents.map((accent) =>
-      generateAccent(sourceFileData, sourceFilePath, accent)
-    )
+  const sourceFileData = (
+    await fs.readFile(_sourceFilePath, {
+      encoding: "utf8",
+    })
+  ).replace(/\$dcommit: .*;/gm, `$dcommit: "(${commit_hash})";`);
+  let tasks = accents.map((accent) =>
+    generateAccent(sourceFileData, sourceFilePath, accent)
   );
+  tasks.push(
+    (async () => {
+      const outputFileName = sourceFilePath;
+      const outputFilePath = path.join(__dirname, outputFileName);
+      await fs.writeFile(outputFilePath, sourceFileData);
+      console.log(`Generated: ${outputFileName}`);
+    })()
+  );
+  return Promise.all(tasks);
 }
 
 // replace brand and write to separate file
